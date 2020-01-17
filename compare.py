df8b97844eaa6f2b0178f3eb7cd2a2791dcb5f8d
michael1241 <michael@jamcupboard.co.uk>
2020-01-17 14:02:57 +0000

    removing rarity calculations

--- compare.py
+++ compare.py
@@ -3,46 +3,12 @@
 import requests
 import chess.pgn
 from collections import defaultdict
-import time
-import pickle
-import os
+
+#API request https://lichess.org/api/games/user/{USER}?max=20&rated=true&ongoing=false&perfType=blitz,rapid,classical
 
 depth = 20
 desiredgames = 20
 
-if os.path.exists('mastersdbcache'):
-    with open('mastersdbcache', 'rb') as db:
-        FENdict = pickle.load(db)
-else:
-    FENdict = {}
-
-def FENrarity(fen, move):
-    cachedfen = FENdict.get(fen)
-    if cachedfen:
-        r = cachedfen
-    else:
-        while True:
-            payload = {'fen': fen, 'topGames': 0, 'moves': 30}
-            r = requests.get(f'https://explorer.lichess.ovh/master', params=payload)
-            if r.status_code == 200:
-                time.sleep(0.25)
-                r = r.json()
-                break
-            if r.status_code == 429:
-                print("waiting 5 seconds")
-                time.sleep(5)
-                continue
-            print(r)
-    FENdict[fen] = r
-    pos_occur = r['white'] + r['draws'] + r['black']
-    for dbmove in r['moves']:
-        if dbmove['uci'] == move:
-            mov_occur = dbmove['white'] + dbmove['draws'] + dbmove['black']
-            rarity =  pos_occur / mov_occur #bigger is rarer
-            return(min(rarity, 1000))
-    return 500 #guess at rare move
-
-#print(checkFEN('rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR', 'e7e5'))
 
 def defineReps(pgnfilepath):
     
@@ -81,26 +47,27 @@ def defineReps(pgnfilepath):
                 break
     return player_moves
 
-rep2 = defineReps('pgns/lichess_somethingpretentious_2020-01-16.pgn')
-#rep1 = defineReps('pgns/lichess_bufferunderrun_2020-01-15.pgn')
-#rep2 = defineReps('pgns/lichess_bufferunderrun_2020-01-15.pgn')
-rep1 = defineReps('pgns/lichess_somethingpretentious_2020-01-15.pgn')
-#rep2 = defineReps('pgns/lichess_somethingpretentious_2020-01-15.pgn')
+library = [
+'pgns/lichess_somethingpretentious_2020-01-16.pgn',
+'pgns/lichess_bufferunderrun_2020-01-15.pgn',
+'pgns/lichess_somethingpretentious_2020-01-15.pgn',
+'pgns/lichess_quirked_2020-01-17.pgn',
+'pgns/lichess_glbert_2020-01-17(1).pgn',
+'pgns/lichess_MMichael_2020-01-17.pgn',
+'pgns/lichess_TheFinnisher_2020-01-17.pgn']
+
+rep1 = defineReps(library[5])
+rep2 = defineReps(library[6])
 
 def compareReps(rep1, rep2):
     score = 0
     rarities = []
     for move in rep1.keys():
-        rarity = FENrarity(move[0], move[1])
-        rarities.append(rarity)
         p2score = rep2.get(move)
         if p2score:
             p1score = rep1[move]
-            score += min(p1score, p2score) * min(p1score, p2score) * rarity
-    potential = sum([x*x*y for x, y in zip(rep1.values(), rarities)])
-    return((score)/potential) 
+            score += min(p1score, p2score) * min(p1score, p2score)
+    potential = sum([x*x for x in rep1.values()])
+    return(score/potential) 
 
 print(compareReps(rep1, rep2))
-
-with open('mastersdbcache', 'wb') as db:
-    pickle.dump(FENdict, db)

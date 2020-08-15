d7c605c846e1da55ce8c8a9ee86cf55165318e97
michael1241 <michael@jamcupboard.co.uk>
2020-08-15 10:50:21 +0100

    new approach started

--- compare.py
+++ compare.py
@@ -2,15 +2,16 @@
 
 import requests
 import chess.pgn
+import numpy as np
+import pandas as pd
 from collections import defaultdict
+from scipy import spatial
 
-#API request https://lichess.org/api/games/user/{USER}?max=100&rated=true&ongoing=false&perfType=blitz,rapid,classical
-
-depth = 20
+depth = 10
 desiredgames = 100
 
 
-def defineReps(pgnfilepath):
+def defineRep(pgnfilepath):
     
     pgn = open(pgnfilepath)
     players = defaultdict(int)
@@ -26,7 +27,7 @@ def defineReps(pgnfilepath):
     def getPlayerColour(game):
         return 0 if game.headers['White'] == player else 1
 
-    player_moves = defaultdict(int)
+    pos_dict = {}
     pgn = open(pgnfilepath)
 
     gamenum = 0
@@ -42,10 +43,16 @@ def defineReps(pgnfilepath):
             board.push(move)
             if (n + player_colour) % 2 != 0: #skip opponents' moves
                 continue
-            player_moves[(storedfen, move.uci())] += 1
+            if pos_dict.get(storedfen):
+                if pos_dict[storedfen].get(move.uci()):
+                    pos_dict[storedfen][move.uci()] += 1
+                else:
+                    pos_dict[storedfen][move.uci()] = 1
+            else:
+                pos_dict[storedfen] = {move.uci(): 1}
             if n > depth:
                 break
-    return player_moves
+    return pos_dict
 
 library = [
 'pgns/lichess_somethingpretentious_2020-01-17_100games.pgn',
@@ -54,18 +61,36 @@ library = [
 'pgns/lichess_TheFinnisher_2020-01-17.pgn',
 'pgns/lichess_MMichael_2020-01-17.pgn']
 
-rep1 = defineReps(library[1])
-rep2 = defineReps(library[2])
+#print(defineRep(library[1]))
 
 def compareReps(rep1, rep2):
-    score = 0
-    rarities = []
-    for move in rep1.keys():
-        p2score = rep2.get(move)
-        if p2score:
-            p1score = rep1[move]
-            score += min(p1score, p2score) * min(p1score, p2score)
-    potential = sum([x*x for x in rep1.values()])
-    return(score/potential) 
+    def standardise(moves1, moves2):
+        m1 = list(moves1.keys())
+        m2 = list(moves2.keys())
+        m1.extend(m2)
+        allmoves = list(set(m1))
+        moves1 = [moves1.get(move, 0) for move in allmoves]
+        moves2 = [moves2.get(move, 0) for move in allmoves]
+        return moves1, moves2
 
-print((compareReps(rep1, rep2) + compareReps(rep2, rep1))/2)
+    fen1 = list(rep1.keys())
+    fen2 = list(rep2.keys())
+    fen1.extend(fen2)
+    allfens = list(set(fen1))
+
+    score = 0
+    for fen in allfens:
+        moves1, moves2 = standardise(rep1.get(fen, {}), rep2.get(fen, {}))
+        cos = 1 - spatial.distance.cosine(moves1, moves2)
+        if not np.isnan(cos):
+            score += cos
+    return score
+
+reps = {}
+for player in library:
+    reps[player] = defineRep(player)
+    print(f'Done {player}')
+
+for player1 in reps:
+    for player2 in reps:
+        print(player1, player2, compareReps(reps[player1], reps[player2]))
